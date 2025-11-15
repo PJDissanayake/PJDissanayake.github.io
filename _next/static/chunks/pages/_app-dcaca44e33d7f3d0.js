ff296ba680c06386a3b413abaccdcad7ec963a1d
PJDissanayake <PJDissanayake@users.noreply.github.com>
2025-11-15 11:54:46 +0000

    deploy: dd67b0cdc752ed74a1ba1d1f1f5f280598f92dbd

---
+++ _next/static/chunks/pages/_app-dcaca44e33d7f3d0.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[636],{12744:(_,n,p)=>{(window.__NEXT_P=window.__NEXT_P||[]).push(["/_app",function(){return p(67809)}])}},_=>{var n=n=>_(_.s=n);_.O(0,[593,792],()=>(n(12744),n(50085))),_N_E=_.O()}]);
